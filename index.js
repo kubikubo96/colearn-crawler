1f32b3f4269ea014180713945907106957f20934
tiennt <tiennt@elcom.com.vn>
2022-06-16 15:15:05 +0700

    "done get data step1"

--- index.js
+++ index.js
@@ -1,27 +1,38 @@
-const HCCrawler = require('headless-chrome-crawler');
-const axios = require('axios').default;
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const crawler = await HCCrawler.launch({
-    // Function to be evaluated in browsers
-    evaluatePage: (() => ({
-      title: $('.title.position-relative').text(),
-    })),
-    // Function to be called with evaluated results from browsers
-    onSuccess: (result => {
-      console.log(result.result);
-      axios({
-        method: 'post',
-        url: 'http://basic.laravel.local/crawler',
-        data: result.result
-      })
-        .then(function (response) {
-          console.log(response.data)
-        });
-    }),
+  const browser = await puppeteer.launch({
+    headless: false,
+    // args: ['--window-size=1900,1000'],
   });
-  await crawler.queue(['https://colearn.vn/']);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 0, height: 0 });
+  await page.goto('https://vungoi.vn/lop-12/bai-tap-mon-toan-s5af3ead5f4ed8c11759c1ade.html');
 
-  await crawler.onIdle(); // Resolved when no queue is left
-  await crawler.close(); // Close the crawler
+  await page.waitForSelector('.chapter-item .sub-string').then(() => {
+    console.log("STEP 1");
+    page.$$eval('.chapter-item .sub-string', (elm) => elm[0].click());
+  });
+
+  await page.waitForSelector('a[href=cau-hoi-13]').then(() => {
+    console.log("STEP 2");
+    page.$eval('a[href=cau-hoi-13]', (elm) => elm.click());
+  });
+
+  await page.waitForSelector('#quiz-solution a').then(() => {
+    console.log("STEP 3");
+    page.$$eval('#quiz-solution a', (elm) => elm[0].click());
+  });
+
+  await page.waitForSelector('#quiz-single .content-quiz').then(() => {
+    console.log("STEP 4");
+    page.$$eval('#quiz-single .content-quiz', (elm) => console.log(elm[0].textContent));
+  });
+
+  await page.waitForSelector('#quiz-single .vn-box-answer .row div').then(() => {
+    console.log("STEP 5");
+    page.$$eval('#quiz-single .vn-box-answer .row div', (elm) => console.log(elm[0].textContent));
+  });
+
+  // await browser.close();
 })();
