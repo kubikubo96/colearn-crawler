9dfcd858622901c64d58361f549a38d8233fd94b
tiennt <tiennt@elcom.com.vn>
2022-06-16 18:48:42 +0700

    "done get data step2"

--- index.js
+++ index.js
@@ -7,32 +7,29 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 0, height: 0 });
-  await page.goto('https://vungoi.vn/lop-12/bai-tap-mon-toan-s5af3ead5f4ed8c11759c1ade.html');
+  const baseUrl = 'https://vungoi.vn'
+  const mathUrl = 'https://vungoi.vn/lop-12/bai-tap-mon-toan-s5af3ead5f4ed8c11759c1ade.html'
+  await page.goto(mathUrl);
+  var chapterNumber = 1;
 
-  await page.waitForSelector('.chapter-item .sub-string').then(() => {
-    console.log("STEP 1");
-    page.$$eval('.chapter-item .sub-string', (elm) => elm[0].click());
-  });
+  const elmChapters = '.list-chapters .chapter-item';
+  while (1) {
+    await page.waitForSelector(elmChapters).then(async () => {
+      await page.$$eval('.chapter-item .sub-string', (element, chapterNumber) => {
+        element[chapterNumber].click()
+      }, chapterNumber);
+      
+      await page.waitForTimeout(2000);
+      await page.goto(mathUrl); // await page.goBack();
 
-  await page.waitForSelector('a[href=cau-hoi-13]').then(() => {
-    console.log("STEP 2");
-    page.$eval('a[href=cau-hoi-13]', (elm) => elm.click());
-  });
+      console.log(chapterNumber);
+      chapterNumber = chapterNumber + 1;
+    });
 
-  await page.waitForSelector('#quiz-solution a').then(() => {
-    console.log("STEP 3");
-    page.$$eval('#quiz-solution a', (elm) => elm[0].click());
-  });
+    await page.waitForTimeout(2000);
+    if (chapterNumber > 5) {
+      break;
+    }
+  }
 
-  await page.waitForSelector('#quiz-single .content-quiz').then(() => {
-    console.log("STEP 4");
-    page.$$eval('#quiz-single .content-quiz', (elm) => console.log(elm[0].textContent));
-  });
-
-  await page.waitForSelector('#quiz-single .vn-box-answer .row div').then(() => {
-    console.log("STEP 5");
-    page.$$eval('#quiz-single .vn-box-answer .row div', (elm) => console.log(elm[0].textContent));
-  });
-
-  // await browser.close();
 })();
