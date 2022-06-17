ba363ad7aa513d251f37613d000f764b6c28fda0
tiennt <tiennt@elcom.com.vn>
2022-06-17 13:50:42 +0700

    "done get data step8"

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     // args: ['--window-size=1900,1000'],
   });
   const page = await browser.newPage();
@@ -23,7 +24,7 @@ const puppeteer = require('puppeteer');
   // var limit_questions = elmQuestions.length - 1;
   var limit_subjects = 0;
   var limit_topics = 0;
-  var limit_questions = 10;
+  var limit_questions = 4;
 
   /**
    * While list menu
@@ -34,13 +35,11 @@ const puppeteer = require('puppeteer');
      */
     try {
       await page.waitForSelector(elmMenus).then(async () => {
-        console.log("Go 1 subject");
         await page.$$eval(elmMenus, (element, subjects_number) => {
           element[subjects_number].click()
         }, subjects_number);
 
         console.log("subjects_number:" + subjects_number);
-        // await page.waitForTimeout(2000);
       });
     } catch (error) {
       page.goto(baseUrl)
@@ -60,9 +59,9 @@ const puppeteer = require('puppeteer');
           }, topics_number);
           if (topics_number >= limit_topics) {
             subjects_number = subjects_number + 1;
+            // await page.goBack()
           }
-          console.log("topics_number:" + topics_number
-          );
+          console.log("topics_number:" + topics_number);
           // await page.waitForTimeout(2000);
         });
       } catch (error) {
@@ -84,6 +83,9 @@ const puppeteer = require('puppeteer');
             if (questions_number >= limit_questions) {
               topics_number = topics_number + 1;
             }
+            console.log("limit_questions:" + limit_questions);
+            console.log("limit_subjects:" + limit_subjects);
+            console.log("limit_topics:" + limit_topics);
             console.log("questions_number:" + questions_number);
             // await page.waitForTimeout(2000);
           });
@@ -96,7 +98,7 @@ const puppeteer = require('puppeteer');
         */
         try {
           await page.waitForSelector('#quiz-single').then(async () => {
-            await page.waitForTimeout(500);
+            await page.waitForTimeout(1000);
             const tempData = await page.evaluate(async () => {
               let temp = [];
               let option = [];
@@ -104,7 +106,7 @@ const puppeteer = require('puppeteer');
               let elmName = document.querySelectorAll('#quiz-single .vn-tit-question strong');
               let elmTag = document.querySelectorAll('#quiz-single .vn-tit-question .clf');
               let elmQuestion = document.querySelectorAll('#quiz-single .content-quiz');
-              let elmImageQuestion = document.querySelectorAll('.question-content .image-group img');
+              let elmImageQuestion = document.querySelectorAll('.question-content .image-item img');
               let elmOption = document.querySelectorAll('.vn-box-answer .row > div');
               let elmSolution = document.querySelectorAll('.content-solution .solution-item p');
               let elmAnswer = document.querySelectorAll('#quiz-solution .solution-item div');
@@ -112,7 +114,7 @@ const puppeteer = require('puppeteer');
               let elmNote = document.querySelectorAll('.content-solution .note');
 
               elmImageQuestion = [...elmImageQuestion]
-              if (imageQuestion.length) {
+              if (elmImageQuestion.length) {
                 imageQuestion = await elmImageQuestion.map(item => ({
                   src: item.getAttribute('src'),
                   title: item.getAttribute('title')
@@ -153,7 +155,9 @@ const puppeteer = require('puppeteer');
 
         //break
         if (questions_number > limit_questions - 1) {
+          questions_number = 0;
           console.log("*******  DONE 1 STEP QUESTON ************* \n");
+          fs.writeFileSync('question_topic_' + topics_number + '.json', JSON.stringify(data));
           await page.goBack()
           break;
         }
@@ -162,6 +166,7 @@ const puppeteer = require('puppeteer');
       //break
       // page.goto(baseUrl)
       if (topics_number > limit_topics - 1) {
+        topics_number = 0;
         console.log("**********  DONE 1 STEP TOPIC *********** \n");
         await page.goBack()
         break;
